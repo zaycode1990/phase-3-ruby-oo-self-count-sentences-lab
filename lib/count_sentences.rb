5fd235fcb58d16501fd7a3e02b82cfc2b3137468
Isaiah <ijandicos90@gmail.com>
2022-07-19 13:25:33 -0400

    Completed Assignments

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+self.end_with?("?")
   end
 
   def exclamation?
-
+self.end_with?("!")
   end
 
   def count_sentences
-
+  self.split(/[?!.]/).reject{|str| str == ""}.count
+  
   end
-end
+end 
+
